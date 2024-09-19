5e8a663eaa1029c750d29a8ccf111d6591b42731
Bhadra Pradeep-Amfoss tasks <bhadrapradeep013@gmail.com>
2024-09-19 17:59:37 +0530

    changes

--- ##Task-2/Arrakis-dex/Eolian Caves/Entrance1/sy.py
+++
@@ -1,3 +0,0 @@
-import os 
-os.system('chmod 400 TheSpiceMelange.txt')
-os.system('chmod g-w parchment.txt')
--- ##Task-2/Arrakis-dex/The Deep Desert E/9/4/Aridale/Voidgate Stronghold/Voidgate.sh
+++
@@ -1,6 +0,0 @@
-echo "Enter code:"
-read -r encoded_input
-
-decoded_output=$(echo "$encoded_input" | base64 -d)
-
-echo -e "\n$decoded_output"
